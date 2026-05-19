21020289eaf7e865534a1d38c700cfa7b7e0fe6e
ShubhiCodess <shubhicodes37@gmail.com>
2026-05-19 23:47:48 +0530

    Authentication added phase 1

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,35 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+// ROUTES
+app.use("/api/auth", authRoutes);
+
+
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
+
+// DATABASE CONNECTION
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
   })
